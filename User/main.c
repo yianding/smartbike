1bbdb73a93af42382b41bbf453d851f14639a62d
yiadning <yianding@gmail.com>
2024-09-23 23:57:35 +0800

    '1'

--- User/main.c
+++ User/main.c
@@ -137,38 +137,7 @@ Delay_ms(1000);
 	Serial_SendString("AT+MPUB=\"4G\",0,0,\"This message send from my stm32\"\r\n");
 	}
 	*/
-		while (1)
-	{
-		if (Serial_RxFlag == 1)
-		{
-			OLED_ShowString(4, 1, "     dd         ");
-			OLED_ShowString(4, 1, Serial_RxPacket);
-			//Serial_SendString("AT+MPUB=\"4G\",0,0,"+Serial_RxPacket);
-		
-			if (strcmp(Serial_RxPacket, "LED_ON") == 0)
-			{
-				LED1_ON();
-				Serial_SendString("LED_ON_OK\r\n");
-				OLED_ShowString(2, 1, "                ");
-				OLED_ShowString(2, 1, "LED_ON_OK");
-			}
-			else if (strcmp(Serial_RxPacket, "LED_OFF") == 0)
-			{
-				LED1_OFF();
-				Serial_SendString("LED_OFF_OK\r\n");
-				OLED_ShowString(2, 1, "                ");
-				OLED_ShowString(2, 1, "LED_OFF_OK");
-			}
-			else
-			{
-				Serial_SendString("ERROR_COMMAND\r\n");
-				OLED_ShowString(2, 1, "                ");
-				OLED_ShowString(2, 1, "ERROR_COMMAND");
-			}
-			
-			Serial_RxFlag = 0;
-		}
-	}
+	
 }
 
 
